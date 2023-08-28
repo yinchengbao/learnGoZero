feea18b6f270fa8fadda5e2fbb1c28ab6d3c209d
yinchengbao <yinchengbao@sinkatec.com>
2023-08-28 10:41:17 +0800

    sql修正

--- sql/User.sql
+++ sql/User.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS `users` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `username` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
